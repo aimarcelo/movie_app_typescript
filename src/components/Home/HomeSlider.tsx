83890bd9ce6d2e7add2f9be55eb31b976db72647
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-10-03 22:01:00 +0800

    Set up carousel component

--- src/components/Home/HomeSlider.tsx
+++ src/components/Home/HomeSlider.tsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import HomeCarousel from './HomeCarousel'
+import { useEffect, useState } from "react";
+import { baseApi } from "../../api/axiosInstance";
+import { CarouselMovie } from "../../utils/constant";
+import HomeCarousel from "./HomeCarousel";
 
 function HomeSlider() {
+  const [carouselMovies, setCarouselMovies] = useState<CarouselMovie[]>([]);
+
+  const fetchUpcoming = async () => {
+    try {
+      const response = await baseApi.get(
+        "/3/movie/upcoming?language=en-US&page=1"
+      );
+      console.log(response.data.results);
+      setCarouselMovies(response.data.results);
+    } catch (err) {
+      console.log("fetch upcoming movies error", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUpcoming();
+  }, []);
+
   return (
-    <div>
-      <HomeCarousel/>
+    <div className="relative w-[900px]">
+      <div id="carouselExample" className="carousel slide">
+        <HomeCarousel carouselMovies={carouselMovies} />
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomeSlider
+export default HomeSlider;
