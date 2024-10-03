dbd7080c6b63f731f892f4be200061f29548cd50
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-10-03 23:46:31 +0800

    Add dynamic feature of carousel list

--- src/components/Home/HomeSlider.tsx
+++ src/components/Home/HomeSlider.tsx
@@ -6,7 +6,34 @@ import HomeCarouselList from "./HomeCarouselList";
 
 function HomeSlider() {
   const [carouselMovies, setCarouselMovies] = useState<CarouselMovie[]>([]);
-  const [next, setNext] = useState<number[]>([1, 2, 3]);
+  const [selected, setSelected] = useState(0);
+  const [next, setNext] = useState<number[]>([]);
+
+  useEffect(() => {
+    if (carouselMovies.length) {
+      const ind1 = (selected + 1) % carouselMovies.length;
+      const ind2 = (selected + 2) % carouselMovies.length;
+      const ind3 = (selected + 3) % carouselMovies.length;
+      setNext([ind1, ind2, ind3]);
+    }
+  }, [carouselMovies, selected]);
+
+  useEffect(() => {
+    const myCarousel = document.getElementById("carouselExample");
+
+    const handleSlide = (event) => {
+      console.log(event.from, "---", event.to);
+      setSelected(event.to);
+    };
+
+    if (myCarousel) {
+      myCarousel.addEventListener("slid.bs.carousel", handleSlide);
+
+      return () => {
+        myCarousel.removeEventListener("slid.bs.carousel", handleSlide);
+      };
+    }
+  });
 
   const fetchUpcoming = async () => {
     try {
