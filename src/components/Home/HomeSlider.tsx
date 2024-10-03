bb87b66d31bebed89c7f26a4cff41a2255fc9662
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-10-03 23:15:22 +0800

    Create carousel for movie list

--- src/components/Home/HomeSlider.tsx
+++ src/components/Home/HomeSlider.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { baseApi } from "../../api/axiosInstance";
 import { CarouselMovie } from "../../utils/constant";
 import HomeCarousel from "./HomeCarousel";
+import HomeCarouselList from "./HomeCarouselList";
 
 function HomeSlider() {
   const [carouselMovies, setCarouselMovies] = useState<CarouselMovie[]>([]);
+  const [next, setNext] = useState<number[]>([1, 2, 3]);
 
   const fetchUpcoming = async () => {
     try {
@@ -23,33 +25,38 @@ function HomeSlider() {
   }, []);
 
   return (
-    <div className="relative w-[900px]">
-      <div id="carouselExample" className="carousel slide">
-        <HomeCarousel carouselMovies={carouselMovies} />
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+    <div className="row">
+      <div className="relative col-8">
+        <div id="carouselExample" className="carousel slide">
+          <HomeCarousel carouselMovies={carouselMovies} />
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className="col-4">
+        <HomeCarouselList next={next} carouselMovies={carouselMovies} />
       </div>
     </div>
   );
