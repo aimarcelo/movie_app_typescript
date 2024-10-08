c06d3a4160102b3f4c35137830085752adde2bb3
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-10-09 00:59:51 +0800

    Make movie Details page design responsive

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -33,28 +33,28 @@ function Details() {
             <img
               src={imagepath + details?.backdrop_path}
               alt="background"
-              className="opacity-40 w-full aspect-[7/4] object-center"
+              className="opacity-40 w-full min-h-[500px] aspect-[7/4] object-center"
             />
             <div className="absolute bottom-0 w-full h-full _carouselGradient"></div>
           </div>
           <div className="absolute top-0 w-full pb-[100px]">
-            <div className="w-[90%] mx-auto mt-[500px]">
-              <div className="flex gap-8">
+            <div className="w-[90%] mx-auto lg:mt-[500px] md:mt-[400px] mt-[200px]">
+              <div className="md:flex gap-8">
                 <img
                   src={imagepath + details?.poster_path}
-                  className="w-[350px] h-fit"
+                  className="lg:w-[350px] md:w-[280px] sm:w-[250px] w-[200px] h-fit"
                 />
-                <div className="text-5xl">
+                <div className="lg:text-5xl md:text-4xl sm:text-3xl text-2xl">
                   <h1>
                     {details?.original_title}
-                    <span className="mx-3 text-4xl">
+                    <span className="md:mx-3 mx-1 lg:text-4xl md:text-3xl sm:text-2xl text-xl">
                       ({details?.release_date?.substring(0, 4)})
                     </span>
                   </h1>
-                  <div className="text-xl text-slate-300 mt-2">
+                  <div className="lg:text-xl md:text-lg sm:text-md text-sm text-slate-300 sm:mt-2 mt-1">
                     <h2>{details?.tagline}</h2>
-                    <h2 className="mt-3">{details?.overview}</h2>
-                    <div className="flex flex-col gap-3 mt-4 text-zinc-300">
+                    <h2 className="sm:mt-3 mt-2">{details?.overview}</h2>
+                    <div className="flex flex-col md:gap-3 gap-2 mt-4 text-zinc-300">
                       <h2>
                         Genres:{" "}
                         {details?.genres?.map((genre) => genre.name).join(", ")}
