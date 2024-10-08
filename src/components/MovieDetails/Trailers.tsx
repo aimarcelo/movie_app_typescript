67239cc9c9df3ab4f441c314c14ab70cfe826b3d
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-10-08 18:24:13 +0800

    Modify Trailers component to render movie trailer

--- src/components/MovieDetails/Trailers.tsx
+++ src/components/MovieDetails/Trailers.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { baseApi } from "../../api/axiosInstance";
+import YouTube from "react-youtube";
 
 function Trailers() {
-  const [trailers, setTrailers] = useState<{ key: string }[]>([]);
+  const [trailers, setTrailers] = useState<{ key: string; name: string }[]>([]);
 
   const fetchTrailers = async () => {
     try {
@@ -21,10 +22,26 @@ function Trailers() {
     fetchTrailers();
   }, []);
 
+  const options = {
+    height: "280",
+    width: "380",
+  };
+
   return (
-    <div className="mt-16">
-      <h1 className="text-3xl text-yellow-500 font-bold">Watch Trailers</h1>
-      {trailers.length > 0 && trailers.map((data) => <h2>{data.key}</h2>)}
+    <div>
+      {trailers.length > 0 && (
+        <div className="mt-16">
+          <h1 className="text-3xl text-yellow-500 font-bold">Watch Trailers</h1>
+          <div className="flex flex-wrap gap-4">
+            {trailers.map((link, ind) => (
+              <div key={ind} className="flex flex-col gap-2 mt-4">
+                <YouTube videoId={link.key} opts={options} />
+                <h1 className="text-xl w-[380px]">{link.name}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
